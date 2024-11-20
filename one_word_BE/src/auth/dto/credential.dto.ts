a54aaaffe7d97a5e920e3bc7e84ccc203084b73a
Phuong Tuan <phuongtuan8797@gmail.com>
2024-11-20 10:09:01 +0700

    completed sign up

--- one_word_BE/src/auth/dto/credential.dto.ts
+++ one_word_BE/src/auth/dto/credential.dto.ts
@@ -1,9 +1,16 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, Matches, MaxLength, maxLength, minLength, MinLength } from "class-validator";
 
 export class AuthCredential {
     @IsNotEmpty()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
     
     @IsNotEmpty()
+    @MinLength(8)
+    @MaxLength(32)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: "Password is too weak",
+    })
     password: string;
 }
--- one_word_BE/src/auth/user.repository.ts
+++ one_word_BE/src/auth/user.repository.ts
@@ -1,18 +1,33 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredential } from "./dto/credential.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from "bcrypt";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
     async createUser(authCredential: AuthCredential): Promise<void>{
         const { username, password } = authCredential;
 
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(password, salt)
+
         const user = this.create({
             username,
-            password,
+            password: hashedPassword,
         });
-
-        this.save(user)
+        
+        try{
+           await this.save(user)
+        } catch(error){
+            if (error.code === "23505") {
+                throw new ConflictException("Uername already exist");
+            }
+            else{
+                throw new InternalServerErrorException();
+            }
+        }
+        
     }
 
 }
