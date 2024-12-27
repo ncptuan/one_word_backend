46675cba423e3ad453492c4de47fbe5885099d65
Phuong Tuan <phuongtuan8797@gmail.com>
2024-12-27 13:31:45 +0700

    restricting getting, update, delete

--- one_word_BE/src/tasks/tasks.repository.ts
+++ one_word_BE/src/tasks/tasks.repository.ts
@@ -8,18 +8,20 @@ import { User } from 'src/auth/user.entity';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
 
     const query = this.createQueryBuilder('task');
 
+    query.andWhere({user})
+
     if (status) {
       query.andWhere('task.status = :status', { status });
     }
-
+// 7eb5ed16-784e-4145-9c3c-1b802d4c26a8
     if (search) {
       query.andWhere(
-        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
         { search: `%${search}%` },
       );
     }
